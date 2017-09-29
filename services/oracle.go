e59936d19dfd2cce58971057512a51c418f72203
wang-x <wang-x@neunn.com>
2017-09-29 12:08:08 +0800

    deleteDatabaseAndUser增加会话查询和kill操作

--- services/oracle.go
+++ services/oracle.go
@@ -129,6 +129,23 @@ func deleteDatabaseAndUser(conn, database, username string) error {
 		return err
 	}
 
+	rows, err := db.Query(fmt.Sprintf("select sid,serial# from v$session where username='%s'", strings.ToUpper(username)))
+	if err != nil {
+		return err
+	}
+	defer rows.Close()
+	for rows.Next() {
+		sid := ""
+		serial := ""
+		err := rows.Scan(&sid, &serial)
+		if err != nil {
+			return err
+		}
+		_, err = db.Query(fmt.Sprintf("alter system kill session '%s,%s'", sid, serial))
+		if err != nil {
+			return err
+		}
+	}
 	_, errDropUser := db.Query(fmt.Sprintf("drop user %s cascade", username))
 	if errDropUser == nil {
 		println("user", username, "was dropped")
